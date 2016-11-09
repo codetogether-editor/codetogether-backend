f2e5f404c9b92a65700fb9ac9537d0f60e7788a2
Michal Muskala <michal@muskala.eu>
2016-11-09 11:33:08 +0100

    Update deploy script

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -5,4 +5,4 @@ set -e
 export MIX_ENV=prod
 mix release --env=prod
 scp rel/codetogether/releases/0.0.1/codetogether.deb "$DEPLOYER@$SERVER":codetogether.deb
-ssh "$DEPLOYER@$SERVER" 'dpkg -i codetogether.deb'
+ssh "$DEPLOYER@$SERVER" 'sudo dpkg -i codetogether.deb'
