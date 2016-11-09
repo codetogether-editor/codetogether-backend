d4c47059961f26c9e658bc47ecdb3cc5074f515a
Michal Muskala <michal@muskala.eu>
2016-11-09 12:53:25 +0100

    Update release tasks

--- lib/release/task.ex
+++ lib/release/task.ex
@@ -1,25 +1,16 @@
 defmodule Codetogether.Release.Task do
+  alias Codetogether.Repo
+
   def migrate do
-    ensure_started(Codetogether.Repo)
+    Application.load(:codetogether)
+    {:ok, _} = Application.ensure_all_started(:ecto)
+    {:ok, _} = Repo.__adapter__.ensure_all_started(Repo, :temporary)
+    {:ok, _} = Repo.start_link(pool_size: 1)
 
     path = Application.app_dir(:codetogether, "priv/repo/migrations")
 
-    Ecto.Migrator.run(Codetogether.Repo, path, :up, all: true)
+    Ecto.Migrator.run(Repo, path, :up, all: true)
 
     :init.stop()
-  end
-
-  defp ensure_started(repo) do
-    {:ok, _} = Application.ensure_all_started(:ecto)
-    {:ok, _} = repo.__adapter__.ensure_all_started(repo, :temporary)
-
-    case repo.start_link(pool_size: 1) do
-      {:ok, pid} ->
-        pid
-      {:error, {:already_started, _pid}} ->
-        nil
-      {:error, error} ->
-        raise "Could not start repo #{inspect repo}, error: #{inspect error}"
-    end
   end
 end
