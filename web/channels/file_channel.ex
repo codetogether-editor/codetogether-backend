dc98da3ec86937ed033c1a4366f0509a2e038d27
Michal Muskala <michal@muskala.eu>
2017-01-11 12:57:43 +0100

    Fix socket channel

--- web/channels/file_channel.ex
+++ web/channels/file_channel.ex
@@ -4,7 +4,7 @@ defmodule Codetogether.FileChannel do
   alias Codetogether.File
 
   def join("file:" <> file_id, _message, socket) do
-    case File.join_file(file_id, socket.assigns.user) do
+    case File.join_file(file_id, socket.assigns.current_user) do
       {:ok, file} ->
         send(self(), :catch_up)
         {:ok, assign(socket, :file, file)}
